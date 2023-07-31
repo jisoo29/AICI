1523ead73155191bf5ec3f50774d52ceda44b3d1
jisoo29 <120074030+jisoo29@users.noreply.github.com>
2023-07-31 14:14:38 +0900

    Delete construction directory

--- construction/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import ConstructionCallTB
-
-
-class ConstructionCallForm(forms.ModelForm):
-    class Meta:
-        model = ConstructionCallTB
-        fields = {"cent", "cstr_file", "cstr_manager", "cstr_num"}
--- construction/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'construction'
-
-urlpatterns = [
-    path('', views.construction_list, name='construction'),
-    path('upload/', views.construction_upload, name='construction_upload'),
-]
